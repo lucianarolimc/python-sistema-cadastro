1fdacea05d0a427ea7438dde4fe7c3d5d69fdf46
lucianarolimc <lucianarolimc@gmail.com>
2025-03-15 09:09:23 -0400

    versão 2 do código - os dados inseridos são exibidos no terminal

--- cadastrar.py
+++ cadastrar.py
@@ -3,13 +3,20 @@ from tkinter import ttk
 import datetime as dt
 
 
-def obter_dados_janela(entrada_dados_descricao, combobox_selecionar_tipo,
-                       entrada_dados_quantidade, lista_linha_dados):
-    
+# lista com opções de unidade
+lista_tipos = ["Galão", "Caixa", "Saco", "Unidade"]
+
+# criar janela
+janela = tk.Tk()
+
+lista_linha_dados = []
+
+def obter_dados_janela():
+
     """
     Obtém os dados de entrada informados na janela de cadastro.
     """
-    
+
     descricao = entrada_dados_descricao.get()
     tipo = combobox_selecionar_tipo.get()
     quantidade = entrada_dados_quantidade.get()
@@ -17,74 +24,58 @@ def obter_dados_janela(entrada_dados_descricao, combobox_selecionar_tipo,
     data_criacao = data_criacao.strftime("%d/%m/%Y %H: %M")
     codigo_id_ = len(lista_linha_dados)+1
     codigo_id_str = "COD-{}".format(codigo_id_)
-    
+
     lista_linha_dados.append((codigo_id_str, descricao, tipo, quantidade, data_criacao))
-    
-    return lista_linha_dados
 
+# título da janela
+janela.title("Ferramenta de cadastro de materiais")
 
-def criar_janela_cadastro(lista_linha_dados):
-    """
-    Cria a janela para realizar o cadastro
-    dos materiais.
-    """
-    
-    # lista com opções de unidade
-    lista_tipos = ["Galão", "Caixa", "Saco", "Unidade"]
+# criar um rótulo de descrição
+rotulo_descricao = tk.Label(text="Descrição do Material")
 
-    # criar janela
-    janela = tk.Tk()
+# posicionar o rótulo dentro da janela
+rotulo_descricao.grid(row=1, column=0, padx=10, pady=10, sticky='nswe', columnspan=4)
 
-    # título da janela
-    janela.title("Ferramenta de cadastro de materiais")
+# criar a entrada de dados para inserir a descrição do material
+entrada_dados_descricao = tk.Entry()
 
-    # criar um rótulo de descrição
-    rotulo_descricao = tk.Label(text="Descrição do Material")
+# adicionar a entrada de dados à janela
+entrada_dados_descricao.grid(row=2, column=0, padx=10, pady=10, sticky='nswe', columnspan=4)
 
-    # posicionar o rótulo dentro da janela
-    rotulo_descricao.grid(row=1, column=0, padx=10, pady=10, sticky='nswe', columnspan=4)
+# criar a rótulo de tipo de unidade
+rotulo_tipo_unidade = tk.Label(text="Tipo da unidade do material")
 
-    # criar a entrada de dados para inserir a descrição do material
-    entrada_dados_descricao = tk.Entry()
+# adicionar o rótulo à janela
+rotulo_tipo_unidade.grid(row=3, column=0, padx=10, pady=10, sticky='nswe', columnspan=2)
 
-    # adicionar a entrada de dados à janela
-    entrada_dados_descricao.grid(row=2, column=0, padx=10, pady=10, sticky='nswe', columnspan=4)
+# criar uma caixa com uma lista de opções
+combobox_selecionar_tipo = ttk.Combobox(values=lista_tipos)
 
-    # criar a rótulo de tipo de unidade
-    rotulo_tipo_unidade = tk.Label(text="Tipo da unidade do material")
+# adicionar o combobox à janela
+combobox_selecionar_tipo.grid(row=3, column=2, padx=10, pady=10, sticky='nswe', columnspan=2)
 
-    # adicionar o rótulo à janela
-    rotulo_tipo_unidade.grid(row=3, column=0, padx=10, pady=10, sticky='nswe', columnspan=2)
+# criar o rótulo da quantidade
+rotulo_qtd = tk.Label(text="Quantidade da unidade de material")
 
-    # criar uma caixa com uma lista de opções
-    combobox_selecionar_tipo = ttk.Combobox(values=lista_tipos)
+# adicionar o rótulo de quantidade à janela
+rotulo_qtd.grid(row=4, column=0, padx=10, pady=10, sticky='nswe', columnspan=2)
 
-    # adicionar o combobox à janela
-    combobox_selecionar_tipo.grid(row=3, column=2, padx=10, pady=10, sticky='nswe', columnspan=2)
+# criar uma entrada de dados de quantidade
+entrada_dados_quantidade = tk.Entry()
 
-    # criar o rótulo da quantidade
-    rotulo_qtd = tk.Label(text="Quantidade da unidade de material")
+# adicionar a entrada de quantidade à janela
+entrada_dados_quantidade.grid(row=4, column=2, padx=10, pady=10, sticky='nswe', columnspan=2)
 
-    # adicionar o rótulo de quantidade à janela
-    rotulo_qtd.grid(row=4, column=0, padx=10, pady=10, sticky='nswe', columnspan=2)
+# criar um botão para submeter as informações
+botao_ok = tk.Button(text="Enviar dados", command=obter_dados_janela)
+# botao_ok = tk.Button(text="Enviar dados")
 
-    # criar uma entrada de dados de quantidade
-    entrada_dados_quantidade = tk.Entry()
+# adicionar o botão ok à janela
+botao_ok.grid(row=5, column=0, padx=10, pady=10, sticky='nswe', columnspan=4)
 
-    # adicionar a entrada de quantidade à janela
-    entrada_dados_quantidade.grid(row=4, column=2, padx=10, pady=10, sticky='nswe', columnspan=2)
+janela.mainloop()
 
-    # criar um botão para submeter as informações
-    botao_ok = tk.Button(text="Enviar dados", command=obter_dados_janela(entrada_dados_descricao, combobox_selecionar_tipo,
-                                                                         entrada_dados_quantidade, lista_linha_dados))
-    # botao_ok = tk.Button(text="Enviar dados")
-
-    # adicionar o botão ok à janela
-    botao_ok.grid(row=5, column=0, padx=10, pady=10, sticky='nswe', columnspan=4)
-
-    print(lista_linha_dados)
-
-    janela.mainloop()
+print(lista_linha_dados)
     
     
     
@@ -97,20 +88,20 @@ def criar_janela_cadastro(lista_linha_dados):
     
     
 
-def main():
+# def main():
     
-    lista_linha_dados = []
+#     lista_linha_dados = []
     
-    # # exibe a janela de cadastro
-    # entrada_dados_descricao, combobox_selecionar_tipo,\
-    # entrada_dados_quantidade = criar_janela_cadastro()
-    criar_janela_cadastro(lista_linha_dados)
+#     # # exibe a janela de cadastro
+#     # entrada_dados_descricao, combobox_selecionar_tipo,\
+#     # entrada_dados_quantidade = criar_janela_cadastro()
+#     criar_janela_cadastro(lista_linha_dados)
     
-    # lista_linha_dados = obter_dados_janela(entrada_dados_descricao, combobox_selecionar_tipo,
-    #                                        entrada_dados_quantidade, lista_linha_dados)
+#     # lista_linha_dados = obter_dados_janela(entrada_dados_descricao, combobox_selecionar_tipo,
+#     #                                        entrada_dados_quantidade, lista_linha_dados)
     
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
 
 
 
